c9f85978342f84d4e21cb85527a8eb3c8f5364fb
rothfralick <11377430+rothfralick@users.noreply.github.com>
2018-12-27 11:35:55 -0500

    Add files via upload

--- chronometer.py
+++ chronometer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from datetime import datetime
+import datetime
 import time
 import os
 import string
@@ -16,9 +16,9 @@ def debug(text,var):
 		print("DEBUG>>> "+text + ": " + str(var))
 
 def getRelativeDate(ordinal,weekday,month,year):
-	firstday = (datetime(year,month,1).weekday() + 1)%7
+	firstday = (datetime.datetime(year,month,1).weekday() + 1)%7
 	firstSunday = (7 - firstday) % 7 + 1
-	return datetime(year,month,firstSunday + weekday + 7*(ordinal-1))
+	return datetime.datetime(year,month,firstSunday + weekday + 7*(ordinal-1))
 
 dateList = 	[
 			["Roth's B'day",	STATIC,10,20],
@@ -42,10 +42,24 @@ timeZoneList = [["GMT",			timezone("GMT")],
 				["India",		timezone("Asia/Kolkata")],
 				["Japan",		timezone("Asia/Tokyo")],
 				["Singapore",	timezone("Singapore")],
-				["UK",			timezone("Europe/London")],
+				["UK",			timezone("Europe/London")]
 				]
+				
+themes =[[colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.white,colors.fg.lightgray,colors.fg.cyan],
+		 [colors.bg.black,colors.fg.lightgreen,colors.fg.red,colors.fg.white]]
+		 
 
-
+#name = " " + chr(0x2665) + " Lori Lollis " + chr(0x2665) + " "
 name = " Roth Fralick "
 
 dbgyear		= 2019
@@ -54,7 +68,7 @@ dbgday		= 5
 dbghour		= 5
 dbgminute	= 5
 dbgsecond	= 5
-dbgstart	= datetime.now()
+dbgstart	= datetime.datetime.now()
 
 SECOND  = 0
 MINUTE  = 1
@@ -79,7 +93,9 @@ timeTable =[["Second",	0,	6],
 
 rows, columns = os.popen('stty size', 'r').read().split()
 rows = int(rows)
-columns = int(columns) - 2
+columns = int(columns)
+
+
 
 def resetCursor():
 	print("\033[0;0H", end="")
@@ -91,51 +107,44 @@ def drawProgressBar(width,min,max,value):
 	level = int(width * (value-min)/(max-min) + .999999999999)
 	return (chr(0x2550) * level + " " * (width-level))
 
-theme =[[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.white, colors.bg.black + colors.fg.white],
-		[colors.bg.black + colors.fg.red, colors.bg.black + colors.fg.green]]		
-
 os.system("clear")
 while True:
-	
-	now = datetime.now()
-	if(dbg):
-		now = (datetime.now()-dbgstart) + \
-			  datetime(dbgyear,dbgmonth,dbgday,dbghour,dbgminute,dbgsecond)
 
-	themeIndex = (now.month-1)%12	
-	vBar = theme[themeIndex][0] + chr(0x2551) + theme[themeIndex][1]
-	hBar = theme[themeIndex][0] + chr(0x2550) + theme[themeIndex][1]
-	vBarUp = theme[themeIndex][0] + chr(0x00af) + theme[themeIndex][1]
-	vBarDown = theme[themeIndex][0] + "_" + theme[themeIndex][1]
-	llCorner = theme[themeIndex][0] + chr(0x0255A) + theme[themeIndex][1]
-	lrCorner = theme[themeIndex][0] + chr(0x0255D) + theme[themeIndex][1]
-	ulCorner = theme[themeIndex][0] + chr(0x02554) + theme[themeIndex][1]
-	urCorner = theme[themeIndex][0] + chr(0x02557) + theme[themeIndex][1]
+	now = datetime.datetime.now()
+	if(dbg):
+		now = (datetime.datetime.now()-dbgstart) + \
+			  datetime.datetime(dbgyear,dbgmonth,dbgday,dbghour,dbgminute,dbgsecond)
+	
+	
 
 	screen = ""
 	output = ""
 	resetCursor()
 
 	uSecond = now.microsecond/1000000
-
+	
+	
+	themeIndex = now.month - 1
+	highlight = [themes[themeIndex][1], themes[themeIndex][3]]
+	print(themes[themeIndex][0],end="")
+	
+	vBar = themes[themeIndex][2] + chr(0x2551) + themes[themeIndex][1]
+	hBar = themes[themeIndex][2] + chr(0x2550) + themes[themeIndex][1]
+	vBarUp = themes[themeIndex][2] + chr(0x00af) + themes[themeIndex][1]
+	vBarDown = themes[themeIndex][2] + "_" + themes[themeIndex][1]
+	llCorner = themes[themeIndex][2] + chr(0x0255A) + themes[themeIndex][1]
+	lrCorner = themes[themeIndex][2] + chr(0x0255D) + themes[themeIndex][1]
+	ulCorner = themes[themeIndex][2] + chr(0x02554) + themes[themeIndex][1]
+	urCorner = themes[themeIndex][2] + chr(0x02557) + themes[themeIndex][1]
+	
 	if (now.month ==12):
 		daysThisMonth = 31
 	else:
-		daysThisMonth = (datetime(now.year,now.month+1,1)- \
-			datetime(now.year,now.month,1)).days
+		daysThisMonth = (datetime.datetime(now.year,now.month+1,1)- \
+			datetime.datetime(now.year,now.month,1)).days
 	
-	dayOfYear = (now - datetime(now.year,1,1)).days
-	daysThisYear = (datetime(now.year+1,1,1) - datetime(now.year,1,1)).days
+	dayOfYear = (now - datetime.datetime(now.year,1,1)).days
+	daysThisYear = (datetime.datetime(now.year+1,1,1) - datetime.datetime(now.year,1,1)).days
 
 	timeTable[SECOND][VALUE]	= now.second + uSecond
 	timeTable[MINUTE][VALUE]	= now.minute + timeTable[SECOND][VALUE]/60
@@ -160,9 +169,9 @@ while True:
 
 	for i in range(len(dateList)):
 		if dateList[i][1] == STATIC:
-			nextDate = datetime(now.year,dateList[i][2],dateList[i][3])
+			nextDate = datetime.datetime(now.year,dateList[i][2],dateList[i][3])
 			if (nextDate-now).total_seconds()<0:
-				nextDate = datetime(now.year+1,dateList[i][2],dateList[i][3])
+				nextDate = datetime.datetime(now.year+1,dateList[i][2],dateList[i][3])
 		else:
 			nextDate = getRelativeDate(dateList[i][2],dateList[i][3],dateList[i][4],now.year)
 			if (nextDate-now).total_seconds()<0:
@@ -192,20 +201,40 @@ while True:
 				" (" + str(nextDate-now).split(".")[0] + ")\n\n"
 		
 	for i in range(0,len(timeZoneList),2):
-		timeStr0 = datetime.now(timeZoneList[i][1]).strftime("%I:%M %p %b %d")
-		timeStr1 = datetime.now(timeZoneList[i+1][1]).strftime("%I:%M %p %b %d")
-		screen += (" {0:>9}: {1:} | ").format(timeZoneList[i][0],timeStr0)
-		screen += (" {0:>9}: {1:}\n").format(timeZoneList[i+1][0],timeStr1)
-
+		time0 = datetime.datetime.now(timeZoneList[i][1])
+		time1 = datetime.datetime.now(timeZoneList[i+1][1])
+	
+		if (time0.weekday() < 5 and time0.hour > 8 and time0.hour < 17):
+			isWorkHours0 = True
+		else:
+			isWorkHours0 = False
+			
+		if (time1.weekday() < 5 and time1.hour > 8 and time1.hour < 17):
+			isWorkHours1 = True
+		else:
+			isWorkHours1 = False
+		debug("weekday", time0.weekday())
+		debug("hour", time0.hour)
+		debug("isWorkHours0", isWorkHours0)
+		#debug("isWorkHours1", isWorkHours1)
+	
+	
+		timeStr0 = highlight[isWorkHours0] + time0.strftime("%I:%M %p %b %d")+themes[themeIndex][1]
+		timeStr1 = highlight[isWorkHours1] + time1.strftime("%I:%M %p %b %d")+themes[themeIndex][1]
+		
+		#+ " "*(len(highlight[isWorkHours0]) + len(highlight[isWorkHours1]) + len(colors.reset.all) + len(colors.reset.all))
+		
+		screen += (" {0:>9}: {1:15} | ").format(timeZoneList[i][0],timeStr0)
+		screen += (" {0:>9}: {1:15}").format(timeZoneList[i+1][0],timeStr1)
+		#screen += " "*2*(len(highlight[isWorkHours0]) + len(highlight[isWorkHours1]) + len(colors.reset.all) + len(colors.reset.all)) + 
+		screen += "\n"
+		
 	#draw a frame
-	output = ulCorner + hBar * (columns) + urCorner + "\n"
-	for line in screen.split("\n"):
-		output += (vBar+"{:"+ str(columns)  +"}"+vBar).format(line) + "\n"
+	
+	
+	screen += "\n" * (rows-screen.count("\n")-3)
 
-	output += (vBar + " "*int(columns) + vBar + "\n")*(rows-screen.count("\n")-3)
-	output += llCorner + hBar * int((columns-len(name))/2-1) + " "+ name + " " + hBar * (columns-(int((columns-len(name))/2-1)) - len(name) -2) + lrCorner
-
-	print(output,end="")
+	print(screen,end="")
 	if dbg:
 		time.sleep(1)
 	else:
