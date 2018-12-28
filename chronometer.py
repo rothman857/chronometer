daf453ac4975d19ae699c672e15cf366c133ec23
rothfralick <11377430+rothfralick@users.noreply.github.com>
2018-12-28 11:20:17 -0500

    cleanup

--- chronometer.py
+++ chronometer.py
@@ -186,8 +186,6 @@ while True:
 			nextDate = getRelativeDate(2,0,3,now.year+1).replace(hour=2)
 	screen += " " + DST[isDaylightSavings][0] + " " + nextDate.strftime("%a %b %d") + \
 				" (" + str(nextDate-now).split(".")[0] + ")\n\n"
-				
-	#screen += vBarDown * columns + "\n"
 		
 	for i in range(0,len(timeZoneList),2):
 		time0 = datetime.datetime.now(timeZoneList[i][1])
@@ -210,7 +208,6 @@ while True:
 		screen += "\n"
 		
 	screen += "\n" * (rows-screen.count("\n")-2)
-	#screen += vBarUp * columns + "\n"
 	screen += hBar * int((columns-len(name))/2) + name + hBar * (columns - int((columns-len(name))/2) - len(name))
 	
 	print(screen,end="")
