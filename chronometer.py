ec0a5cfe81cdb95ab329aa7f07ceaa16d2ccf34a
rothfralick <11377430+rothfralick@users.noreply.github.com>
2018-12-28 11:18:22 -0500

    extended highlighting to timezone name

--- chronometer.py
+++ chronometer.py
@@ -203,10 +203,10 @@ while True:
 		else:
 			isWorkHours1 = False
 
-		timeStr0 = highlight[isWorkHours0] + time0.strftime("%I:%M %p %b %d")+themes[themeIndex][1]
-		timeStr1 = highlight[isWorkHours1] + time1.strftime("%I:%M %p %b %d")+themes[themeIndex][1]
-		screen += (" {0:>9}: {1:15}  "+vBar+" ").format(timeZoneList[i][0],timeStr0)
-		screen += (" {0:>9}: {1:15}").format(timeZoneList[i+1][0],timeStr1)
+		timeStr0 = time0.strftime("%I:%M %p %b %d")
+		timeStr1 = time1.strftime("%I:%M %p %b %d")
+		screen += highlight[isWorkHours0] + (" {0:>9}: {1:15}  "+vBar+" ").format(timeZoneList[i][0],timeStr0) + themes[themeIndex][1]
+		screen += highlight[isWorkHours1] + (" {0:>9}: {1:15}").format(timeZoneList[i+1][0],timeStr1) + themes[themeIndex][1]
 		screen += "\n"
 		
 	screen += "\n" * (rows-screen.count("\n")-2)
