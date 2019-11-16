5eb2042863d40c97c4f2e28329fee90b32611a71
roth <11377430+rothfralick@users.noreply.github.com>
2019-11-16 11:52:23 -0500

    launch.sh init

--- chronometer.py
+++ chronometer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 from datetime import datetime, timedelta
 import time
 import os
---
+++ launch.sh
@@ -0,0 +1,4 @@
+while ! ping -c 1 -W 1 8.8.8.8 do
+    echo "Waiting for network..."
+    sleep 1
+done
