12c3ac3bd2266a5693f65d8a123bf602d17cdf0b
rothman857 <roth@roth>
2019-11-16 17:05:47 -0500

    added launcher

--- launch.sh
+++ launch.sh
@@ -1,5 +1,11 @@
-while ! ping -c 1 -W 1 8.8.8.8 do
+while ! ping -c 1 -W 1 8.8.8.8; do
     echo "Waiting for network..."
     sleep 1
 done
-. chronometer.py
+
+script_dir="$(dirname $0)"
+cd $script_dir
+git pull
+python3 chronometer.py
+cd -
+sleep 10
