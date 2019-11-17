cb1dc344436b6c96e44207c14a9afabb09bf1a1f
roth <11377430+rothfralick@users.noreply.github.com>
2019-11-17 00:07:08 -0500

    dsf

--- launch.sh
+++ launch.sh
@@ -2,18 +2,19 @@
 # Call this script from /etc/rc.local to update on reboot
 
 
-#Wait for internet connection
+# Wait for internet connection
 while :
 do
   sleep 1
-  if (ping -c 1 -W 1 8.8.8.8 > /dev/null); then
+#  if (ping -c 1 -W 1 8.8.8.8 > /dev/null); then
+if (nc -zw1 google.com 80); then
     break
   else
     echo "Waiting for network..."
   fi
 done
 
-#Pull most recent code
+# Pull most recent code
 script_dir="$(dirname $0)"
 cd $script_dir
 git pull
