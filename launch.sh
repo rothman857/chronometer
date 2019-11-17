43db67bd52620eac816b5bb142f24808604939af
rothman857 <roth@roth>
2019-11-16 23:01:59 -0500

    fixed launch.sh

--- launch.sh
+++ launch.sh
@@ -1,11 +1,18 @@
-while ! ping -c 1 -W 1 8.8.8.8; do
-    echo "Waiting for network..."
-    sleep 1
+#!/usr/bin/sh
+
+#Wait for internet connection
+while :
+do
+  sleep 1
+  if (ping -c 1 -W 1 8.8.8.8 > /dev/null); then
+    break
+  else
+  fi
 done
 
+#Pull most recent code
 script_dir="$(dirname $0)"
 cd $script_dir
 git pull
+
 python3 chronometer.py
-cd -
-sleep 10
