5c1b410138179df675b7a08373c4825a5da9483d
roth <rothman857@gmail.com>
2019-12-23 21:04:20 -0500

    flipped countdown signs

--- chronometer.py
+++ chronometer.py
@@ -559,7 +559,7 @@ def main():
                 hours, remainder = divmod(abs(s), 3600)
                 minutes, seconds = divmod(remainder, 60)
                 subs = 100000 * (seconds - int(seconds))
-                sign = '-' if s < 0 else ' '
+                sign = ' ' if s < 0 or i == 3 else '+'
                 time_List[i] = '{}{:02}:{:02}:{:02}.{:05}'.format(sign, int(hours), int(minutes), int(seconds), int(subs))
 
             leap_stats = ["LS:" + time_List[0],
