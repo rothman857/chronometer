2f4b3a21debbaaff3b5859d0fd0e1e7ce0305447
rfralick <rfralick@gmail.com>
2022-04-14 09:20:26 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chronometer-rothman857",
+    version="1.0.0",
+    author="rothman857",
+    author_email="rothman857@gmail.com",
+    description="Chronometer Time Display",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rothman857/chronometer",
+    project_urls={
+        "Bug Tracker": "https://github.com/rothman857/chronometer/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
