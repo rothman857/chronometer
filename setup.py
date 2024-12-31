8792020b328c429a8ca2fee7fb8bc694cf3d4410
rfralick <rfralick@vmware.com>
2024-12-31 11:36:39 -0500

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info.minor == 6:
 
 setuptools.setup(
     name="chronometer",
-    version="1.0.4",
+    version="1.0.5",
     author="rothman857",
     author_email="rothman857@gmail.com",
     description="Chronometer Time Display",
